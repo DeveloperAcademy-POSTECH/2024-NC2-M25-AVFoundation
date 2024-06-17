26d5afe5068ab586b56e62d4d85530608a0e58f6
won <mosiccan@gmail.com>
2024-06-17 11:22:34 +0900

    Create: Xcode 프로젝트 생성

---
+++ NC2-AudioPlayer/NC2-AudioPlayer/NC2_AudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC2_AudioPlayerApp.swift
+//  NC2-AudioPlayer
+//
+//  Created by Woowon Kang on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct NC2_AudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
